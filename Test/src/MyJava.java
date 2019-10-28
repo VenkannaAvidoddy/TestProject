7d2617f9422899d4c231937e7f89992d2f1077d7
enky <Venky@DESKTOP-8RPBF6O>
2019-10-28 21:18:21 +0530

    JustNow

--- Test/src/MyJava.java
+++ Test/src/MyJava.java
@@ -4,6 +4,8 @@ public class MyJava {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
 		System.out.println("Love you raja");
+		System.out.println("new git changes made by me");
+		System.out.println("my life my wish");
 
 	}
 
